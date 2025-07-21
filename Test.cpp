017ef3f0a60fcc39d49ad15ca75db5d5a92ff5c2
web3-human <mkhum4n@gmail.com>
2025-07-21 23:13:08 +0530

    Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int a = 10, b = 5;
+
+	cout << "sum = "<<(a+b) << endl;
+	cout << "diff = "<<(a+b) << endl;
+	cout << "multiply = "<<(a+b) << endl;
+	cout << "product = "<<(a+b) << endl;
+
+	cout << "SHA256 will be outdate soon";
+
+	return 0;
+}
