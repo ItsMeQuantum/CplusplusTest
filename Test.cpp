d2491c97da289a6002780d6616cc232312bc8955
web3-human <mkhum4n@gmail.com>
2025-07-21 23:32:18 +0530

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -6,9 +6,9 @@ int main() {
 	int a = 10, b = 5;
 
 	cout << "sum = "<<(a+b) << endl;
-	cout << "diff = "<<(a+b) << endl;
-	cout << "multiply = "<<(a+b) << endl;
-	cout << "product = "<<(a+b) << endl;
+	cout << "diff = "<<(a-b) << endl;
+	cout << "multiply = "<<(a*b) << endl;
+	cout << "product = "<<(a/b) << endl;
 
 	cout << "SHA256 will be outdate soon";
 
